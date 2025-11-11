7b2ac6673f8bec659ce81731ae36f9a18457c076
Themi Sadas <darksadasytnew@gmail.com>
2025-11-11 08:14:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ function convertToBool(text, fault = 'true') {
 
 //gg
 module.exports = {
-SESSION_ID: 'VISPER-MD&h84eTPitTL',
+SESSION_ID: 'VISPER-MD&MGcy9dYvgI',
 ANTI_DELETE: process.env.ANTI_DELETE === undefined ? 'true' : process.env.ANTI_DELETE, 
 MV_BLOCK: process.env. MV_BLOCK === undefined ? 'true' : process.env. MV_BLOCK,    
 ANTI_LINK: process.env.ANTI_LINK === undefined ? 'true' : process.env.ANTI_LINK, 
 SEEDR_MAIL: '',
 SEEDR_PASSWORD: '',
 SUDO: '',//
-DB_NAME: 'pramaaaataaaaaaaaaaaajjaa',
+DB_NAME: 'pramaaaataaaaaaaaaaaaajjaa',
 LANG: 'SI',
 OWNER_NUMBER: '94724884317',
 TG_GROUP: 'https://t.me/+Zm865mJ_TL0yNGVl'
